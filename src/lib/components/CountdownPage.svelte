f4be7459f01f6320067294275d9a28475cc1fdeb
pontakornth <pontakorn61@gmail.com>
2023-12-07 14:14:15 +0700

    Refactor countdown into a page component

---
+++ src/lib/components/CountdownPage.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	export let targetDate: Date = new Date();
+	export let passedText = 'Passed';
+	export let countdownText = `Countdown to ${targetDate.toLocaleDateString('en-GB')}`;
+	let currentDate = new Date();
+	$: timeDelta = targetDate.getTime() - currentDate.getTime();
+	$: remainingDays = Math.floor(timeDelta / (1000 * 60 * 60 * 24));
+	$: remainingHours = Math.floor((timeDelta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	$: remainingMinutes = Math.floor((timeDelta % (1000 * 60 * 60)) / (1000 * 60));
+	$: remainingSeconds = Math.floor((timeDelta % (1000 * 60)) / 1000);
+	$: isPassed = timeDelta <= 0;
+	let interval: number;
+	let isError = false;
+
+	onMount(() => {
+		// I do not take milliseconds into account right now.
+		// I am not sure if making currentDate becomes reactive value will impact performance or not.
+		interval = setInterval(() => {
+			currentDate = new Date();
+			if (isPassed) {
+				clearInterval(interval);
+				return;
+			}
+		}, 1000);
+	});
+</script>
+
+<main
+	class="flex p-4 text-center bg-orange-500 min-h-screen justify-center items-center flex-col text-white"
+>
+	{#if isError}
+		<p>Invalid time</p>
+	{:else}
+		<h1 class="text-5xl font-bold">{countdownText}</h1>
+		{#if isPassed}
+			<p class="text-lg">{passedText}</p>
+		{:else}
+			<p class="text-3xl">
+				{remainingDays} Day(s) {remainingHours} Hour(s) {remainingMinutes} Minute(s) {remainingSeconds}
+				Second(s)
+			</p>
+		{/if}
+	{/if}
+</main>
--- src/routes/[year]/[[month=month]]/[[day=day]]/+page.svelte
+++ src/routes/[year]/[[month=month]]/[[day=day]]/+page.svelte
@@ -1,44 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
+	import CountdownPage from '$lib/components/CountdownPage.svelte';
 	export let targetDate: Date = $page.data.targetDate;
-	let currentDate = new Date();
-	$: timeDelta = targetDate.getTime() - currentDate.getTime();
-	$: remainingDays = Math.floor(timeDelta / (1000 * 60 * 60 * 24));
-	$: remainingHours = Math.floor((timeDelta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	$: remainingMinutes = Math.floor((timeDelta % (1000 * 60 * 60)) / (1000 * 60));
-	$: remainingSeconds = Math.floor((timeDelta % (1000 * 60)) / 1000);
-	$: isPassed = timeDelta <= 0;
-	let interval: number;
-	let isError = false;
-
-	onMount(() => {
-		// I do not take milliseconds into account right now.
-		// I am not sure if making currentDate becomes reactive value will impact performance or not.
-		interval = setInterval(() => {
-			currentDate = new Date();
-			if (isPassed) {
-				clearInterval(interval);
-				return;
-			}
-		}, 1000);
-	});
 </script>
 
-<main
-	class="flex p-4 text-center bg-orange-500 min-h-screen justify-center items-center flex-col text-white"
->
-	{#if isError}
-		<p>Invalid time</p>
-	{:else}
-		<h1 class="text-5xl font-bold">Countdown to {targetDate.toLocaleDateString('en-GB')}</h1>
-		{#if isPassed}
-			<p class="text-lg">Passed</p>
-		{:else}
-			<p class="text-3xl">
-				{remainingDays} Day(s) {remainingHours} Hour(s) {remainingMinutes} Minute(s) {remainingSeconds}
-				Second(s)
-			</p>
-		{/if}
-	{/if}
-</main>
+<CountdownPage {targetDate} />
