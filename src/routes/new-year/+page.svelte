6c6310f2bf36a04accf6cdd721dc40cf97942f90
pontakornth <pontakorn61@gmail.com>
2023-12-07 14:14:21 +0700

    Add new year countdown

---
+++ src/routes/new-year/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import CountdownPage from '$lib/components/CountdownPage.svelte';
+
+	const currentDate = new Date();
+	const newYearDate = new Date(
+		currentDate.getMonth() == 0 && currentDate.getDate() == 1
+			? currentDate.getFullYear()
+			: currentDate.getFullYear() + 1,
+		0,
+		1
+	);
+</script>
+
+<CountdownPage
+	targetDate={newYearDate}
+	countdownText="Countdown to New Year"
+	passedText="Happy New Year"
+/>
